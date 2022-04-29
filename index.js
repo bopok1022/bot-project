f59ad303a483939aa630442641a841e58b5cc9f8
hello <104561387+bopok1022@users.noreply.github.com>
2022-04-29 02:50:23 +0000

    hrhtrjtyj

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 const client = new Discord.Client ()
 
-client.on("ready", => {
+client.on("ready", () => {
   console.log(`logged in as ${client.user.tag`)
 })
 
