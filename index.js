45a272b0baff4cc1b56ef1ea4d4946a752a5183b
hello <104561387+bopok1022@users.noreply.github.com>
2022-04-30 12:39:08 +0000

    many

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-const Discord = require("discord.js")
-const Client = new Discord.Client ()
+const keep_alive = require('./event/keep_alive.js')
+const Discord = require(`discord.js`);
+const { Intents, Message, Collection, bot } = require("discord.js");
+const client = new Discord.Client({
+	intents: [Intents.FLAGS.GUILDS]
+});
+const { token } = require('./config.json')
+const { codeBlock } = require("@discordjs/builders");
+async function clean(client, text) {
+	if (text && text.constructor.name == "Promise")
+		text = await text;
+	if (typeof text !== "string")
+		text = require("util").inspect(text, { depth: 1 });
 
-Client.on("ready", () => {
-  console.log(`logged in as ${Client.user.tag`)
-})
+	text = text
+		.replace(/`/g, "`" + String.fromCharCode(8203))
+		.replace(/@/g, "@" + String.fromCharCode(8203));
 
-Client.on("message", msg =>{
- if (msg.content === "ping") {    
-   msg.reply("pong")
-  } 
-})
+	text = text.replaceAll(client.token, "[REDACTED]");
 
-Client.login(process.env.TOKEN)
+	return text;
+};
+
+client.on("ready", () => {
+	console.log(`logged in as ${client.user.tag}!`)
+});
+
+if (Message.content === "hello there") {
+	Message.reply("GENERAL KENOBI")
+} else if (Message.content === "hi") {
+	console.log("someone is using hi")
+  Message.reply("hello")
+} else if (Message.content === "hello") {
+	Message.reply("hello")
+} else {
+	console.log('bug')
+}
+
+
+client.login(token);
